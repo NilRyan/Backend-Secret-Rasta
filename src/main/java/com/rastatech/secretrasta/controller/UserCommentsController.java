09edb8a8290c8e54d9a1bd4e890de1be6742a54f
Neil Ryan L <neilryanlipaod@gmail.com>
2021-11-29 23:01:02 +0800

    IMPLEMENTED UserCommentsController

--- src/main/java/com/rastatech/secretrasta/controller/UserCommentsController.java
+++ src/main/java/com/rastatech/secretrasta/controller/UserCommentsController.java
@@ -1,10 +1,37 @@
 package com.rastatech.secretrasta.controller;
 
+import com.rastatech.secretrasta.dto.CommentResponse;
+import com.rastatech.secretrasta.model.CommentEntity;
+import com.rastatech.secretrasta.service.CommentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users/{user_id}/comments")
 public class UserCommentsController {
+
+    private ModelMapper modelMapper;
+    private CommentService commentService;
+
+    @GetMapping
+    public List<CommentResponse> fetchCommentsByUser(@PathVariable("user_id") Long userId) {
+        List<CommentEntity> comments = commentService.fetchCommentsByUser(userId);
+        return comments.stream().map(this::mapToCommentResponse).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{comment_id}")
+    public CommentResponse fetchCommentByUser(@PathVariable("user_id") Long userId, @PathVariable("comment_id") Long commentId) {
+        return mapToCommentResponse(commentService.fetchCommentByUser(userId, commentId));
+    }
+    private CommentResponse mapToCommentResponse(CommentEntity comment) {
+        return modelMapper.map(comment, CommentResponse.class);
+    }
 
 }
