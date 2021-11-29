b943756fe66b1d9037d12e985d2bc7116145b73e
Philip <philipcalapezz@gmail.com>
2021-11-29 11:01:29 +0800

    Initial commit

---
+++ src/main/java/com/rastatech/secretrasta/SecretrastaApplication.java
@@ -0,0 +1,13 @@
+package com.rastatech.secretrasta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretrastaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretrastaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rastatech/secretrasta/SecretrastaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rastatech.secretrasta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretrastaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
