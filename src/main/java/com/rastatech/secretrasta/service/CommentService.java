3b91d5ed30244f0f553e40d3cf7bce6eab9e280f
Neil Ryan L <neilryanlipaod@gmail.com>
2021-11-30 16:25:13 +0800

    IMPLEMENTED CommentServiceImpl

--- src/main/java/com/rastatech/secretrasta/service/CommentService.java
+++ src/main/java/com/rastatech/secretrasta/service/CommentService.java
@@ -10,8 +10,6 @@ public interface CommentService {
     CommentEntity createComment(Long wishId, CommentRequest comment);
     List<CommentEntity> fetchCommentsByWish(Long wishId);
     List<CommentEntity> fetchCommentsByUser(Long userId);
-    CommentEntity fetchCommentByWish(Long wishId, Long commentId);
-    CommentEntity fetchCommentByUser(Long userId, Long commentId);
-    void updateComment(Long wishId, Long commentId, UpdateCommentRequest comment);
-    void deleteComment(Long wishId, Long commentId);
+    void updateComment(Long commentId, UpdateCommentRequest comment);
+    void deleteComment(Long commentId);
 }
