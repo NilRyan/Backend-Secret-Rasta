c8116e2d757ad959857c2aadedcfa2686a23056f
Neil Ryan L <neilryanlipaod@gmail.com>
2021-11-30 14:07:15 +0800

    IMPLEMENTED UserServiceImpl
    Added model mapper for mapping DTOs to entities and allowing null
    values.

--- src/main/java/com/rastatech/secretrasta/service/UserServiceImpl.java
+++ src/main/java/com/rastatech/secretrasta/service/UserServiceImpl.java
@@ -4,18 +4,19 @@ import com.rastatech.secretrasta.dto.UpdateUserRequest;
 import com.rastatech.secretrasta.model.UserEntity;
 import com.rastatech.secretrasta.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Scanner;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public List<UserEntity> fetchUsers() {
@@ -28,12 +29,15 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserEntity updateUser(Long userId, UpdateUserRequest user) {
-
+    public UserEntity updateUser(Long userId, UpdateUserRequest updateUserRequest) {
+        UserEntity user = fetchUser(userId);
+        modelMapper.map(updateUserRequest, user);
+        return userRepository.save(user);
     }
 
     @Override
     public void deleteUser(Long userId) {
-
+        if (!userRepository.existsById(userId)) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        userRepository.deleteById(userId);
     }
 }
