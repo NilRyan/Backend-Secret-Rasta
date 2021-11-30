7045ed2a7d81eb885a7aa08c81ee2f662710af00
Neil Ryan L <neilryanlipaod@gmail.com>
2021-11-30 13:52:09 +0800

    ADDED configuration for model mapper
    Enabled setSkipNull for updating properties with null.

--- src/main/java/com/rastatech/secretrasta/service/UserServiceImpl.java
+++ src/main/java/com/rastatech/secretrasta/service/UserServiceImpl.java
@@ -2,23 +2,34 @@ package com.rastatech.secretrasta.service;
 
 import com.rastatech.secretrasta.dto.UpdateUserRequest;
 import com.rastatech.secretrasta.model.UserEntity;
+import com.rastatech.secretrasta.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Scanner;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
     @Override
     public List<UserEntity> fetchUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public UserEntity fetchUser(Long userId) {
-        return null;
+        return userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @Override
     public UserEntity updateUser(Long userId, UpdateUserRequest user) {
-        return null;
+
     }
 
     @Override
