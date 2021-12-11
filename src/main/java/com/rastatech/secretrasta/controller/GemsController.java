30c43ff1ee6bf6e38588d9505668506d93e9932e
Neil Ryan L <neilryanlipaod@gmail.com>
2021-12-11 10:57:20 +0800

    ADDED GemsController to aggregate add and send gems

--- src/main/java/com/rastatech/secretrasta/controller/GemsController.java
+++ src/main/java/com/rastatech/secretrasta/controller/GemsController.java
@@ -1,7 +1,9 @@
 package com.rastatech.secretrasta.controller;
 
 import com.rastatech.secretrasta.dto.request.AddBalanceRequest;
+import com.rastatech.secretrasta.dto.request.SendGemsRequest;
 import com.rastatech.secretrasta.service.AddBalanceService;
+import com.rastatech.secretrasta.service.SendGemsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -9,15 +11,25 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/addbalance")
+@RequestMapping("/api/rastagem")
 @RequiredArgsConstructor
-public class AddBalanceController {
+public class GemsController {
 
     private final AddBalanceService addBalanceService;
+    private final SendGemsService sendGemsService;
 
-    @PostMapping
+    @PostMapping("/add")
     public void addBalance(@Valid @RequestBody AddBalanceRequest addBalanceRequest, Authentication auth) {
         String username = (String) auth.getPrincipal();
         addBalanceService.addBalance(username, addBalanceRequest);
     }
+
+    @PostMapping("/transfer/{user_id}")
+    public void sendRastaGemsToUser(@PathVariable("user_id") Long sendToUserId,
+                                    @Valid @RequestBody SendGemsRequest sendGemsRequest,
+                                    Authentication auth ) {
+        String username = (String) auth.getPrincipal();
+        sendGemsService.sendGemsToUser(sendToUserId, username, sendGemsRequest);
+    }
+
 }
